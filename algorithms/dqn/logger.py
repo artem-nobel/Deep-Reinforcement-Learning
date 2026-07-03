b2667c1501c301ee33078a2bfccda44ec2e99fec
artem_nobel <artem_khorkow@icloud.com>
2026-07-04 00:42:15 +0300

    def print_plot in utils.py

--- algorithms/dqn/logger.py
+++ algorithms/dqn/logger.py
@@ -3,28 +3,6 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-# def print_plot(reward_history, mean_reward_history):
-# def print_plot(config, reward_history, mean_reward_history):
-#     print('max reward', max(reward_history))
-#     print('mean reward', np.mean(reward_history))
-#
-#     print(f"Mean last 100: {np.mean(reward_history[-100:]):.1f}")
-#
-#     plt.plot(reward_history, label="reward_history", color="red")
-#     plt.plot(mean_reward_history, label="mean_reward_history", color="blue")
-#     plt.title("dqn_duel_per")
-#     plt.text(0., 500, f'LR = {config.learning_rate:.4f}, gamma = {config.gamma:.3f}')
-#     plt.text(0., 450, f'PER : ALPHA = {config.alpha:.2f}')
-#
-#     plt.text(0., 400, f'Seed = {config.seed:.2f}, target_up = {config.target_update_freq:.2f}')
-#     plt.text(0., 350,f"Mean last 100: {np.mean(reward_history[-100:]):.1f}")
-#
-#
-#     plt.ylim(-400, 600)
-#     plt.legend()
-#     plt.grid(True)
-#     plt.show()
-
 def log_params(config):
     # with mlflow.start_run(run_name='dqn_duel_tree'):
         mlflow.log_params({
