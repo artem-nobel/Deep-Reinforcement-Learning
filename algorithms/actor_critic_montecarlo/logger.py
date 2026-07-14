75c6746a2169b779fbd33488d0e66ac4c5f2a77b
artem_nobel <artem_khorkow@icloud.com>
2026-07-15 00:27:36 +0300

    Update actor_critic_montecarlo

---
+++ algorithms/actor_critic_montecarlo/logger.py
@@ -0,0 +1,33 @@
+import mlflow
+import numpy as np
+
+
+def log_params(config):
+
+    mlflow.log_params({
+        "learning_rate": config.learning_rate,
+        "gamma": config.gamma,
+        "n_episodes": config.n_episodes,
+        "max_step": config.max_step,
+        "seed": config.seed,
+        "env_name": config.env_name
+    })
+
+
+def log_metrics(agent, episode, reward, reward_history, loss_info=None):
+    # if not mlflow.active_run():
+    #     mlflow.start_run()
+
+    metrics = {
+        "episode_reward": reward,
+        "loss": agent.last_loss,
+        "actor_loss": agent.last_actor_loss,
+        "critic_loss": agent.last_critic_loss,
+        "grad_norm": agent.last_grad_norm,
+        "mean_reward_100": np.mean(reward_history[-100:])
+    }
+
+    if loss_info:
+        metrics.update(loss_info)
+
+    mlflow.log_metrics(metrics, step=episode)
---
+++ algorithms/actor_critic_montecarlo/main.py
@@ -0,0 +1,79 @@
+import gymnasium as gym
+import mlflow
+from matplotlib import pyplot as plt
+from agent import Agent
+from config import Config
+from trainer import Trainer
+from utils import get_env_info, set_seed
+
+
+mlflow.set_tracking_uri("http://127.0.0.1:8080")
+mlflow.set_experiment("lunar_lander_actor_critic")
+
+
+def main():
+
+    config = Config()
+
+    set_seed(config.seed)
+
+    env = gym.make(
+        config.env_name,
+        render_mode="human"
+        if config.render
+        else None
+    )
+
+    env.reset(
+        seed=config.seed
+    )
+
+    n_states, n_actions = get_env_info(
+        env
+    )
+
+    agent = Agent(
+        config=config,
+        n_states=n_states,
+        n_actions=n_actions,
+    )
+
+    trainer = Trainer(
+        env=env,
+        agent=agent,
+        config=config,
+    )
+
+    with mlflow.start_run():
+        reward_history, mean_reward_history = trainer.train()
+    # reward_history, mean_reward_history = trainer.train()
+
+    env.close()
+
+    plt.figure(
+        figsize=(12, 5)
+    )
+
+    plt.plot(
+        reward_history
+    )
+
+    plt.title(
+        "Actor-Critic training"
+    )
+
+    plt.xlabel(
+        "Episode"
+    )
+
+    plt.ylabel(
+        "Reward"
+    )
+
+    plt.grid()
+
+    plt.show()
+
+if __name__ == "__main__":
+
+    main()
